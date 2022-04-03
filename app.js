b04b4d00170750d946e8a5230bd82b0774ef24d0
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-04-03 21:10:37 +0200

    express server port 8000

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+
+
+
+const port = process.env.PORT || 8000;
+app.listen(port, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", port);
+})
